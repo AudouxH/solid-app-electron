f9f34eb07b31141ff3196618ea96d08fc6188275
Audouxh <hugo.audoux@epitech.eu>
2022-11-16 15:00:37 +0100

    add solid app into electron by url localhost:3000

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>Salut je suis une application solid executé dans electron</p>
+    </div>
+  );
+}
+
+export default App;
